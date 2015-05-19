b86f420829381790b37b626878ff591fe4b0675e
Cynrick Chiu <cyn.2k5@outlook.com>
2015-05-19 07:33:21 +0000

    Add tests for users controller and comments controller

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "create comment success" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    place = FactoryGirl.create(:place)
+    assert_difference 'place.comments.count' do
+      post :create, :place_id => place.id, :comment => {
+        :message => 'Very yummy!',
+      	:rating => '5_stars'
+      }
+    end
+    assert_equal 1, place.comments.count
+    assert_redirected_to place_path(place)
+  end
+
+  test "create comment not logged in" do
+    post :create, :place_id => 'abc'
+    assert_redirected_to new_user_session_path
+  end
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "show found" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    get :show, :id => user.id
+    assert_response :success
+  end
+
+  test "show not found" do
+    user = FactoryGirl.create(:user)
+    assert_raises(ActiveRecord::RecordNotFound) do
+      get :show, :id => 'abc'
+    end
+  end
+
 end
