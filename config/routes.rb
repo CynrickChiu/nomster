3d5b47eb01cccfdbf9829904721b30f00fd8587b
Cynrick Chiu <cyn.2k5@outlook.com>
2015-07-16 23:41:39 +0000

    Generate serializer for place model, generate api/places controller, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Nomster::Application.routes.draw do
     end
   end
 
+  namespace :api, :defaults => {:format => :json} do
+    resources :places, :only => [:index, :show]
+  end
+
   resources :users, :only => :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
