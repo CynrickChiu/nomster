443abe088a3d2befe47614a19842bd2ae8ec3a8e
Cynrick Chiu <cyn.2k5@outlook.com>
2016-02-12 09:56:01 -0800

    Edit spacing

--- db/migrate/20150504011000_alter_places_add_user_id_column.rb
+++ db/migrate/20150504011000_alter_places_add_user_id_column.rb
@@ -1,6 +1,6 @@
 class AlterPlacesAddUserIdColumn < ActiveRecord::Migration
   def change
-  	add_column :places, :user_id, :integer
-  	add_index :places, :user_id
+    add_column :places, :user_id, :integer
+    add_index :places, :user_id
   end
 end
